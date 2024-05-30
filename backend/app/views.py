e311e3f6f684899bcbc51ab2f6d3bb9374eea980
Nuwan Rathnayaka <nuwanrathnayaka@Nuwans-iMac.local>
2024-05-30 12:14:28 +0800

    basic project setup

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['POST'])
+def login_view(request):
+    return Response({'message':'Hello'})
