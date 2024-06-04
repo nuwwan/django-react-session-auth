650229afb8ba9b4ab0a7411eef2197e41f60ef37
Nuwan Rathnayaka <nuwanrathnayaka@Nuwans-iMac.local>
2024-06-04 23:14:21 +0800

    prettier formattings

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,22 +1,22 @@
-import React from "react"
-import { Route, Routes as Router } from "react-router-dom"
-import Home from "./pages/auth/Home"
-import Register from "./pages/auth/Register"
-import Login from "./pages/auth/Login"
-import ProtectedRoute from "./components/ProtectedRoute"
-import User from "./pages/auth/User"
+import React from 'react'
+import { Route, Routes as Router } from 'react-router-dom'
+import Home from './pages/auth/Home'
+import Register from './pages/auth/Register'
+import Login from './pages/auth/Login'
+import ProtectedRoute from './components/ProtectedRoute'
+import User from './pages/auth/User'
 
 const Routes = () => {
-    return (
-        <Router>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route element={<ProtectedRoute />}>
-                <Route path='/user' element={<User />} />
-            </Route>
-        </Router>
-    )
+  return (
+    <Router>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/user" element={<User />} />
+      </Route>
+    </Router>
+  )
 }
 
 export default Routes
